761c3d7d24476ceef386e0f53880bf01fc05833a
lMarco-Dev <marco.dev2408@gmail.com>
2026-02-26 17:56:42 -0500

    FRONTEND RADAR EMSI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        ¡RADAR EMSI está vivo! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
